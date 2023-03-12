21e0b8cae6b8e9f9183d43e05d74019c8244a407
johnascott <js@heyorca.com>
2023-03-12 15:39:14 -0300

    Hello World

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/pages/Home/Home';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home/>
     </View>
   );
 }
@@ -17,4 +16,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native"
+
+const Home: React.FC = () => {
+
+    return (
+        <Text>Hello World</Text>
+    )
+}
+
+export default Home
